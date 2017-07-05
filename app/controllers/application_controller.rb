30ae0591bd8431fff4109e9063f7e514f7f803e7
Lin Chen <cl_fighting@163.com>
2017-07-05 12:57:13 +0000

    add cart action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to :back,notice: "add to cart"
+  end
 end
